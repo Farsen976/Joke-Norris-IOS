afbbb1db1a74e92838358967598fae7468aa2b21
Developer <developer@Mac-de-Developer.local>
2019-02-01 15:51:49 +0100

    🎉 chore: start project

---
+++ Joke Norris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Joke Norris
+//
+//  Created by Developer on 01/02/2019.
+//  Copyright © 2019 Farsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
